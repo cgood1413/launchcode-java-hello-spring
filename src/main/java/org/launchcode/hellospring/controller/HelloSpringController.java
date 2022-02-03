64f78b5a6ecbd6f3c00d49bb7fedc7c77e52e8e4
Chance Goodwin <cgood1413@gmail.com>
2022-02-02 21:59:16 -0600

    Chapter 10 Exercise Completion

--- src/main/java/org/launchcode/hellospring/controller/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controller/HelloSpringController.java
@@ -5,28 +5,31 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
-@RequestMapping("hello")
 public class HelloSpringController {
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloSpring(@RequestParam String name){
-        return "Hello, " + name;
-    }
-
-    @GetMapping("{name}")
-    public String helloSpringPathParam(@PathVariable String name){
-        return "Hello, " + name;
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloSpring(@RequestParam String name, @RequestParam String lang){
+        String greeting = lang.equals("en") ? "Hello" : lang.equals("fr") ? "Bonjour" : lang.equals("es") ? "Hola" : lang.equals("ita") ? "Buongiorno" : lang.equals("ger") ? "Guten tag" : "Hello";
+        return greeting + ", " +  name + "!";
     }
 
     @GetMapping("form")
     public String helloForm(){
         return "<html>" +
                 "<body>" +
-                    "<form action='hello' method='post'>" +
-                    "<input type='text' name='name'>" +
-                    "<button type='submit'>Submit</button>" +
+                    "<form action='hello' method='get'>" +
+                        "<input type='text' name='name'>" +
+                            "<select name='lang'>" +
+                                "<option value='en'>English</option>" +
+                                "<option value='fr'>Français</option>" +
+                                "<option value='es'>Español</option>" +
+                                "<option value='ita'>Italiano</option>" +
+                                "<option value='ger'>Deutsch</option>" +
+                            "</select>" +
+                        "<button type='submit'>Submit</button>" +
                     "</form>" +
                 "</body>" +
                 "</html>";
     }
+
 }
