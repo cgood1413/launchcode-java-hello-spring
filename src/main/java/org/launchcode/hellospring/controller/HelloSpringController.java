05afcb08a84529ae13f5fa916ca76fb72e3c13a1
Chance Goodwin <cgood1413@gmail.com>
2022-02-15 20:51:53 -0600

    Chapter 11 Intro to Thymeleaf Completion

--- src/main/java/org/launchcode/hellospring/controller/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controller/HelloSpringController.java
@@ -1,32 +1,39 @@
 package org.launchcode.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloSpringController {
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloSpring(@RequestParam String name){
-        return "Hello, " + name;
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloSpring(@RequestParam String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
-    @GetMapping("{name}")
-    public String helloSpringPathParam(@PathVariable String name){
-        return "Hello, " + name;
+    @GetMapping("hello/{name}")
+    public String helloSpringPathParam(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                    "<form action='hello' method='post'>" +
-                    "<input type='text' name='name'>" +
-                    "<button type='submit'>Submit</button>" +
-                    "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloName(Model model){
+        List<String> names = new ArrayList<>(Arrays.asList("Chance", "Kallie", "Bella"));
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
